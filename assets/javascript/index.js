28807e80fcdcc9e603d6c9e231420543e62b30f6
ccho0415 <ccho0415@gmail.com>
2016-12-19 14:24:18 -0500

    looked at the guardians website and had a good idea on how to do transitions from one section of the website to the next. classes!

---
+++ assets/javascript/index.js
@@ -0,0 +1,10 @@
+$("#about-side-nav-bar").on("click", function(){
+	$("body").removeClass();
+	$("body").addClass("aboutMe");
+});
+
+$("#project-side-nav-bar").on("click", function(){
+	$("body").removeClass();
+	$("body").addClass("projects");
+
+});
